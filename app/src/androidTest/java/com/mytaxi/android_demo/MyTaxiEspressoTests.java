aa0c1cb7cb18ad1eb1f21868521fed47b9b450d1
Vivek Shringi <vivshr@Viveks-MacBook-Pro.local>
2019-04-28 09:54:17 +0200

    added grant permission rule

--- app/src/androidTest/java/com/mytaxi/android_demo/MyTaxiEspressoTests.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MyTaxiEspressoTests.java
@@ -1,13 +1,7 @@
 package com.mytaxi.android_demo;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.mytaxi.android_demo.activities.MainActivity;
-
-import android.content.Context;
-import android.support.test.espresso.action.ViewActions;
+import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -17,27 +11,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.InstrumentationRegistry;
-import androidx.test.rule.GrantPermissionRule;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
@@ -53,6 +26,8 @@ public class MyTaxiEspressoTests {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule
             = new ActivityTestRule<>(MainActivity.class);
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
     @Before
     public void setActivity() {
@@ -77,7 +52,7 @@ public class MyTaxiEspressoTests {
 
    @Test
     public void test003_Logout() {
-       loginActions.logout();
+        loginActions.logout();
     }
 
 }
